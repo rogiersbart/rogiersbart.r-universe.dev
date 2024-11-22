426e817d9e8f1cf64e3bc36a72da84739a5726f9
rogiersbart <bart.rogiers@sckcen.be>
2024-11-22 10:34:29 +0100

    Retry RDCOMClient

--- packages.R
+++ packages.R
@@ -4,25 +4,26 @@ df <- tibble::tribble(
          ~package, ~url, ~branch, ~subdir,
 
   # personal
-            "bro",                  rb("bro"),                   NA, NA,
+            "bro",                      rb("bro"),                   NA, NA,
 
   # infrastructure
-            "std",                  rb("std"),                   NA, NA,
-            "rui",                  rb("rui"),                   NA, NA,
-             "ra",                   rb("ra"),                   NA, NA,
-             "ti",                   rb("ti"),                   NA, NA,
-  "spectralscale",        rb("spectralscale"),                   NA, NA,
+            "std",                      rb("std"),                   NA, NA,
+            "rui",                      rb("rui"),                   NA, NA,
+             "ra",                       rb("ra"),                   NA, NA,
+             "ti",                       rb("ti"),                   NA, NA,
+  "spectralscale",            rb("spectralscale"),                   NA, NA,
 
   # hydro
-       "RMODFLOW",             rb("RMODFLOW"),                   NA, NA,
-        "RMT3DMS",              rb("RMT3DMS"),                   NA, NA,
-       "RMODPATH",     gh("cneyens/RMODPATH"),                   NA, NA,
+       "RMODFLOW",                 rb("RMODFLOW"),                   NA, NA,
+        "RMT3DMS",                  rb("RMT3DMS"),                   NA, NA,
+       "RMODPATH",         gh("cneyens/RMODPATH"),                   NA, NA,
 
   # microstructure
-          "nmhsa",                rb("nmhsa"),                   NA, NA,
-     "morphology",           rb("morphology"),                   NA, NA,
+          "nmhsa",                    rb("nmhsa"),                   NA, NA,
+     "morphology",               rb("morphology"),                   NA, NA,
 
   # dependencies not available elsewhere on r-universe
-        "raylibr",              rb("raylibr"), "prefix-class-names", NA,
+        "raylibr",                  rb("raylibr"), "prefix-class-names", NA,
+    "RDCOMClient", gh("BSchamberger/RDCOMClient"),                   NA, NA,
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
